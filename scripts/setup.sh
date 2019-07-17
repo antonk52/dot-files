16cba7242455f14ffaa518fb40860305a46500dc
antonk52 <antonk52@yandex-team.ru>
2019-07-18 00:42:42 +0300

    [scripts] setup init

---
+++ scripts/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# symlink .vim & .vimrc to ~/config/nvim & ~/config/nvim/init.vim respectively
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+if [ ! -d ~/.vim ]; then
+  mkdir ~/.vim
+fi
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+
+ln -s ~/dot-files/.zshrc ~/.zshrc
+ln -s ~/dot-files/.tmux.conf ~/.tmux.conf
+
+echo 'symlinks created'
