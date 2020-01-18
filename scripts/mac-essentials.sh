0a4c31f5b20cf3dfe0b610ec7550d14c002f6114
antonk52 <antonk52@yandex-team.ru>
2020-01-18 20:00:32 +0300

    [mac] essential settings
    \# easy way to check the setting after toggling in GUI
    defaults read > ./def.txt
    \# change stuff in settings.app
    defaults read > ./mod.txt
    git diff --no-index ./mod.txt ./def.txt

--- scripts/mac-essentials.sh
+++ scripts/mac-essentials.sh
@@ -1,3 +1,7 @@
+#
+# Reference
+# https://github.com/kevinSuttle/macOS-Defaults/blob/master/REFERENCE.md
+#
 # Save screenshots in ~/Screenshots
 if [ ! -d ~/Screenshots ]; then
   mkdir ~/Screenshots
@@ -5,6 +9,20 @@ if [ ! -d ~/Screenshots ]; then
 fi
 echo 'changed screencapture location'
 defaults write com.apple.screencapture location ~/Screenshots
+
+# enable dragging using tree fingers on touchpad
+defaults write com.apple.AppleMultitouchTrackpad DragLock = 0
+defaults write com.apple.AppleMultitouchTrackpad Dragging = 0
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag = 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad DragLock = 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Dragging = 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag = 1
+
+# dock settings
+defaults write com.apple.dock autohide = 1
+defaults write com.apple.dock orientation = 'left'
+defaults write com.apple.dock tilesize = 52
+defaults write com.apple.dock show-recents = 0
 
 # repeat keys on hold more often
 echo 'set key repeat to 2'
