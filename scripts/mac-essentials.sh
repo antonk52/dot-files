c4ab1c88a361bf62068b6884e9d2aa21c4542d8b
antonk52 <antonk52@yandex-team.ru>
2020-02-26 17:20:54 +0300

    [mac] enable os keyboard navigation by default

--- scripts/mac-essentials.sh
+++ scripts/mac-essentials.sh
@@ -7,7 +7,7 @@ if [ ! -d ~/Screenshots ]; then
   mkdir ~/Screenshots
   echo 'created ~/Screenshots'
 fi
-echo 'changed screencapture location'
+echo 'Screenshots will be saved to ~/Screenshots'
 defaults write com.apple.screencapture location ~/Screenshots
 
 # enable dragging using tree fingers on touchpad
@@ -24,11 +24,16 @@ defaults write com.apple.dock orientation = 'left'
 defaults write com.apple.dock tilesize = 52
 defaults write com.apple.dock show-recents = 0
 
+# enable keyboard navigation in os applications with tab/shift+tab
+echo 'MacOS keyboard navigation enabled'
+default write "Apple Global Domain" AppleKeyboardUIMode = 2
+
 # repeat keys on hold more often
-echo 'set key repeat to 2'
+echo 'Set key repeat to 2'
 defaults write -g KeyRepeat -int 2
-echo 'set intitinal key repeat to 15'
+echo 'Set intitinal key repeat to 15'
 defaults write -g InitialKeyRepeat -int 15
 
 # remap capslock key to escape
+echo 'CapsLock key now acts like an Esc key'
 hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029}]}'
