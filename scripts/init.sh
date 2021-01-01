57650353b86e53a9101348a5139f7c051b25c066
antonk52 <halloy52@gmail.com>
2021-01-02 01:12:37 +0300

    [setup] ask to perform a thing during setup

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,13 +5,22 @@
 # runs the expected scripts in the intended order
 #
 
+ask_for() {
+    read -p "$1 [y/n]" -n 1 -r
+    echo ''
+    if [[ $REPLY == 'y' ]]
+    then
+        source "$2"
+    fi
+}
+
 echo 'Init started'
 
 cd scripts
 
-source ./prepare-xdg.sh
-source ./mac-essentials.sh
-source ./brew.sh
+ask_for "prepare XDG envs?" "./prepare-xdg.sh"
+ask_for "prepare macos essentials?" "./mac-essentials.sh"
+ask_for "install brew and other apps with it?" "./brew.sh"
 
 cd ..
 
