e458aff8885bc219f9bc77f778f54ff204b674ea
antonk52 <halloy52@gmail.com>
2021-03-22 11:35:19 +0300

    [npm] list deps vertically

--- scripts/prepare-npm.sh
+++ scripts/prepare-npm.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-npm install -g n npmrc synd serve neoovim yarn lerna veendor
+# install global node dependencies
 
+npm install -g \
+    lerna \ # monorepo management
+    n \ # switch node versions
+    neoovim \ # editor dependency
+    npmrc \ # switch between different npmrc
+    pkg \ # bundle node cli into an executable
+    serve \ # local web server for static files
+    synd \ # rsync wrapper
+    veendor \ # install deps as an archive
+    yarn
