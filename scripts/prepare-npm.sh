22c4f6901e8f42fc7c726e2185d0801ec2e75e81
antonk52 <halloy52@gmail.com>
2021-04-24 18:28:52 +0300

    [npm] install global npm deps one by one
    there's an issue with \

--- scripts/prepare-npm.sh
+++ scripts/prepare-npm.sh
@@ -2,13 +2,12 @@
 
 # install global node dependencies
 
-npm install -g \
-    lerna \ # monorepo management
-    n \ # switch node versions
-    neoovim \ # editor dependency
-    npmrc \ # switch between different npmrc
-    pkg \ # bundle node cli into an executable
-    serve \ # local web server for static files
-    synd \ # rsync wrapper
-    veendor \ # install deps as an archive
-    yarn
+npm i -g lerna    # monorepo management
+npm i -g n        # switch node versions
+npm i -g neovim   # editor dependency
+npm i -g npmrc    # switch between different npmrc
+npm i -g pkg      # bundle node cli into an executable
+npm i -g serve    # local web server for static files
+npm i -g synd     # rsync wrapper
+npm i -g veendor  # install deps as an archive
+npm i -g yarn
