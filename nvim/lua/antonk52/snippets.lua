e79483b14fb1b332d79f00b2039747bac2ae4a20
antonk52 <halloy52@gmail.com>
2021-10-22 00:20:44 +0100

    [vim] use $0 in snippets where possible

--- nvim/lua/antonk52/snippets.lua
+++ nvim/lua/antonk52/snippets.lua
@@ -18,7 +18,7 @@ end
 local javascript_snippets = {
     fun = snippets.u.match_indentation([[
 function ${1}(${2}) {
-    $3
+    $0
 }]]);
     us = [["use strict";]];
     ["switch"] = snippets.u.match_indentation([[
@@ -28,19 +28,15 @@ switch (${1:condition}) {
     case ${4:cond}:
         ${5:expr}
     default:
-        ${6:expr}
+        $0
 }
 ]]);
 
-    ["con"] = [[console.log(${1:logme})]];
+    ["con"] = [[console.log($0)]];
 
-    ["iif"] = snippets.u.match_indentation([[
-/* istanbul ignore file */
-    ]]);
+    ["iif"] = snippets.u.match_indentation([[/* istanbul ignore file */]]);
 
-    ["iin"] = snippets.u.match_indentation([[
-/* istanbul ignore next */
-    ]]);
+    ["iin"] = snippets.u.match_indentation([[/* istanbul ignore next */]]);
 
     ["ednl"] = snippets.u.force_comment([[eslint-disable-next-line ${1:rule-name}]]);
 
@@ -50,9 +46,7 @@ switch (${1:condition}) {
 
     ["ee"] = snippets.u.force_comment([[\$ExpectError<'${1:why is it expected?}'>]]);
 
-    ["import"] = snippets.u.match_indentation([[
-import ${2:thing} from '${1:package}';
-    ]]);
+    ["import"] = snippets.u.match_indentation([[import ${2:thing} from '${1:package}';]]);
 
     -- see `guard` exaple in the docs
     -- https://github.com/norcalli/snippets.nvim#usage
@@ -63,14 +57,12 @@ const [${1:state}, set${|S[1]:match"^.":upper()}${|S[1]:gsub("^.", "")}] = useSt
     ["useEffect"] = snippets.u.match_indentation([[
 useEffect(() => {
     ${1:logic}
-}, [${2:leave_empty_for_componentDidMount}]);
-    ]]);
+}, [${2:leave_empty_for_componentDidMount}]);]]);
 
     ["useCallback"] = snippets.u.match_indentation([[
 useCallback(() => {
     ${1:logic}
-}, [${2:dependencies}]);
-]]);
+}, [${2:dependencies}]);]]);
 }
 
 local default_snippets = {
@@ -95,26 +87,26 @@ local default_snippets = {
     };
     lua = {
         req = [[local ${2:${1|S.v:match"([^.()]+)[()]*$"}} = require '$1']];
-        fun = [[function (${1}) ${2} end]];
-        lfun = [[local function ${1}(${2}) ${3} end]];
+        fun = [[function (${1}) $0 end]];
+        lfun = [[local function ${1}(${2}) $0 end]];
         ["loc"] = [[local ${1} = ${2}]];
         ["if"] = snippets.u.match_indentation [[
 if ${1} then
-    $2
+    $0
 end]];
         ["ifel"] = snippets.u.match_indentation [[
 if ${1} then
     ${2}
 else
-    ${3}
+    $0
 end]];
         ["for"] = snippets.u.match_indentation [[
 for ${1:k}, ${2:v} in ipairs(${3}) do
-    $4
+    $0
 end]];
         ["fori"] = snippets.u.match_indentation [[
 for ${1:i}, ${2:v} in ipairs(${3}) do
-    $4
+    $0
 end]];
     };
     markdown = {
@@ -122,24 +114,21 @@ end]];
 | First Header  | Second Header |
 | ------------- | ------------- |
 | Content Cell  | Content Cell  |
-| Content Cell  | Content Cell  |
-        ]]);
+| Content Cell  | Content Cell  |]]);
 
-        ["link"] = [[[${1:text}](${2:url})]];
+        ["link"] = [[[${1:text}]($0)]];
 
-        ["img"] = [[![${1:alt}](${2:url})]];
+        ["img"] = [[![${1:alt}]($0)]];
 
         ["details"] = snippets.u.match_indentation([[
 <details><summary>${1:tldr}</summmary>
-${2:body}
-</details>
-        ]]);
+$0
+</details>]]);
 
         ["todo"] = snippets.u.match_indentation([[
 ## TODO
 
-- [ ] ${1:do this}
-        ]]);
+- [ ] $0]]);
 
 
         ["tags"] = snippets.u.match_indentation([[
@@ -147,39 +136,34 @@ ${2:body}
 tags: [${1:tag_name}]
 ---
 
-${2:content}
-        ]]);
+$0]]);
 
         ["fm"] = snippets.u.match_indentation([[
 ---
 ${1:front_matter}
 ---
 
-${2:content}
-        ]]);
+$0]]);
 
-        ["b"] = [[**${1:bold}**]];
+        ["b"] = [[**$0**]];
 
-        ["i"] = [[_${1:italic}_]];
+        ["i"] = [[_$0_]];
 
-        ["cross"] = [[~~${1:crossed}~~]];
+        ["cross"] = [[~~$0~~]];
     };
     vim = {
         ["fun"] = snippets.u.match_indentation([[
-        fun! ${1:Function_name}($2)
-            ${3:body}
-        endf
-        ]]);
+fun! ${1:Function_name}($2)
+    $0
+endf]]);
 
         ["if"] = snippets.u.match_indentation([[
-        if ${1:condition}
-            ${2:body}
-        endif
-        ]]);
+if ${1:condition}
+    $0
+endif
+]]);
 
-        ["autocmd"] = snippets.u.match_indentation([[
-        autocmd ${1:Event} ${2:pattern} ${3:++once} ${4:++nested} ${5:YOUR_COMMAND_HERE}
-        ]]);
+        ["autocmd"] = snippets.u.match_indentation([[autocmd ${1:Event} ${2:pattern} ${3:++once} ${4:++nested} ${5:YOUR_COMMAND_HERE}]]);
 
     };
     ["javascript"] = javascript_snippets;
@@ -193,22 +177,19 @@ ${2:content}
 local jest_snippets = merge(javascript_snippets, {
     ["desc"] = snippets.u.match_indentation([[
 describe('${1:what are we testing}', () => {
-    $3
-});
-]]);
+    $0
+});]]);
     ["it"] = snippets.u.match_indentation([[
 it('${1:what to test}', () => {
     const result = ${2:funcName}(${3:args});
     const expected = ${4:'what do we expect?'};
 
     expect(result).toEqual(expected);
-});
-]]);
+});]]);
     ["mock"] = snippets.u.match_indentation([[
 jest.mock('${1:file/path/to/mock}', () => ({
-    ${2:exportedFunc}: jest.fn($3),
-}));
-]]);
+    ${2:exportedFunc}: jest.fn($0),
+}));]]);
 })
 
 function M.set_snippets_for_filetype()
@@ -255,7 +236,8 @@ function M.setup()
     snippets.snippets = default_snippets
 
     vim.cmd [[autocmd BufEnter * lua require'antonk52.snippets'.set_snippets_for_filetype()]]
-end
 
+    vim.cmd [[command EditSnippets edit ~/.config/nvim/lua/antonk52/snippets.lua]]
+end
 
 return M
