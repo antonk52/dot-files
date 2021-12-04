5d109ec8f7759031f8d1f07aed15a18cdfd04017
antonk52 <halloy52@gmail.com>
2021-12-04 00:19:39 +0000

    [vim] env var to optout from treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -516,34 +516,36 @@ vim.defer_fn(
 vim.defer_fn(function() require'colorizer'.setup() end, 300)
 
 -- treesitter {{{2
-vim.defer_fn(function()
-    -- if you get "wrong architecture error
-    -- open nvim in macos native terminal app and run `:TSInstall`
-    require "nvim-treesitter.configs".setup {
-        ensure_installed = {},
-        highlight = { enable = true },
-        playground = {
-            enable = true,
-            disable = {},
-            -- Debounced time for highlighting nodes in the playground from source code
-            updatetime = 25,
-            -- Whether the query persists across vim sessions
-            persist_queries = false,
-            keybindings = {
-                toggle_query_editor = 'o',
-                toggle_hl_groups = 'i',
-                toggle_injected_languages = 't',
-                toggle_anonymous_nodes = 'a',
-                toggle_language_display = 'I',
-                focus_language = 'f',
-                unfocus_language = 'F',
-                update = 'R',
-                goto_node = '<cr>',
-                show_help = '?',
-            },
+if vim.env.TREESITTER ~= '0' then
+    vim.defer_fn(function()
+        -- if you get "wrong architecture error
+        -- open nvim in macos native terminal app and run `:TSInstall`
+        require "nvim-treesitter.configs".setup {
+            ensure_installed = {},
+            highlight = { enable = true },
+            playground = {
+                enable = true,
+                disable = {},
+                -- Debounced time for highlighting nodes in the playground from source code
+                updatetime = 25,
+                -- Whether the query persists across vim sessions
+                persist_queries = false,
+                keybindings = {
+                    toggle_query_editor = 'o',
+                    toggle_hl_groups = 'i',
+                    toggle_injected_languages = 't',
+                    toggle_anonymous_nodes = 'a',
+                    toggle_language_display = 'I',
+                    focus_language = 'f',
+                    unfocus_language = 'F',
+                    update = 'R',
+                    goto_node = '<cr>',
+                    show_help = '?',
+                },
+            }
         }
-    }
-end, 100)
+    end, 100)
+end
 
 -- bad-practices.nvim {{{2
 vim.cmd('command! BadPracticesSetup lua require("bad_practices").setup()')
