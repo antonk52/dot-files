6c727e76d8b161c4119c58e8e73a0701d8caa244
antonk52 <halloy52@gmail.com>
2022-01-20 22:59:06 +0000

    [npm] get npm config values from git config

--- scripts/prepare-npm.sh
+++ scripts/prepare-npm.sh
@@ -8,6 +8,14 @@ if [ ! -d ~/.npm-global ]; then
     mkdir ~/.npm-global
 fi
 
+# newly created packages should be MIT licensed
+npm config set init.license MIT
+
+npm config set init.version 0.1.0
+npm config set init.author.email $(git config --get user.email)
+npm config set init.author.name $(git config --get user.name)
+npm config set init.author.url "https://github.com/antonk52"
+
 # install global node dependencies
 
 npm i -g lerna    # monorepo management
@@ -19,4 +27,6 @@ npm i -g serve    # local web server for static files
 npm i -g synd     # rsync wrapper
 npm i -g typescript-language-server
 npm i -g veendor  # install deps as an archive
+npm i -g vscode-langservers-extracted # html/json/css/eslint LSPs
 npm i -g yarn
+
