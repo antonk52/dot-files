b9f1e8295d160eb24f988f0c286f71fbacd417b8
antonk52 <halloy52@gmail.com>
2022-11-07 00:51:39 +0000

    [vim] maybe fix stylua gh action

--- nvim/ftplugin/qf.lua
+++ nvim/ftplugin/qf.lua
@@ -2,5 +2,5 @@
 vim.keymap.set('n', '<cr>', '<cr>:cclose<cr>:echo<cr>', { buffer = 0 })
 -- remove item from quickfix
 vim.keymap.set('n', 'dd', function()
-    require"antonk52.quickfix".remove_item()
+    require('antonk52.quickfix').remove_item()
 end, { buffer = 0 })
