aed009cb75a1923c03981fecc500c2cab675b1a1
antonk52 <halloy52@gmail.com>
2023-01-05 19:02:01 +0000

    [vim] refactor markdown surrounder logic

--- nvim/lua/antonk52/markdown.lua
+++ nvim/lua/antonk52/markdown.lua
@@ -19,93 +19,78 @@ function M.lookup_word_under_cursor()
     vim.cmd('silent !open dict://' .. word)
 end
 
-local function make_surrounder_function(surrounder)
+local function surrender(pos_start, pos_end, before, after)
+    local start_line = vim.api.nvim_buf_get_lines(0, pos_start[1] - 1, pos_start[1], true)[1]
+
+    local is_same_line = pos_start[1] == pos_end[1]
+    if is_same_line then
+        local pre_selection = string.sub(start_line, 1, pos_start[2])
+        local the_selection = string.sub(start_line, pos_start[2] + 1, pos_end[2] + 1)
+        local post_selection = string.sub(start_line, pos_end[2] + 2)
+
+        local surrounder_len = #before + #after
+
+        local is_removing = vim.startswith(the_selection, before)
+        and vim.endswith(the_selection, after)
+        and #the_selection > surrounder_len
+        if is_removing then
+            local sub = string.sub(the_selection, 1 + #before, -1 - #after)
+            start_line = pre_selection .. sub .. post_selection
+        else
+            start_line = pre_selection .. before .. the_selection .. after .. post_selection
+        end
+
+        vim.api.nvim_buf_set_lines(0, pos_start[1] - 1, pos_start[1], true, { start_line })
+    else
+        local end_line = vim.api.nvim_buf_get_lines(0, pos_end[1] - 1, pos_end[1], true)[1]
+        local pre_end_line = string.sub(end_line, 1, pos_end[2] + 1)
+        local post_end_line = string.sub(end_line, pos_end[2] + 2)
+
+        local pre_start_line = string.sub(start_line, 1, pos_start[2])
+        local post_start_line = string.sub(start_line, pos_start[2] + 1)
+
+        vim.pretty_print({pre_start_line, post_start_line})
+        vim.pretty_print({pre_end_line, post_end_line})
+
+        local first = vim.startswith(post_start_line, before)
+        local last = vim.endswith(pre_end_line, after)
+        local is_removing = first and last
+
+        if is_removing then
+            -- remove **
+            start_line = pre_start_line .. string.sub(post_start_line, 1 + #before)
+            end_line = string.sub(pre_end_line, 1, -1 - #after) .. post_end_line
+        else
+            -- add **
+            start_line = pre_start_line .. before .. post_start_line
+            end_line = pre_end_line .. after .. post_end_line
+        end
+
+        vim.api.nvim_buf_set_lines(0, pos_start[1] - 1, pos_start[1], true, { start_line })
+        vim.api.nvim_buf_set_lines(0, pos_end[1] - 1, pos_end[1], true, { end_line })
+    end
+end
+local function make_surrounder_function(before, after)
     return function()
         -- {line, col}
         local pos_start = vim.api.nvim_buf_get_mark(0, '<')
         -- {line, col}
         local pos_end = vim.api.nvim_buf_get_mark(0, '>')
 
-        local start_line = vim.api.nvim_buf_get_lines(0, pos_start[1] - 1, pos_start[1], true)[1]
-
-        local is_same_line = pos_start[1] == pos_end[1]
-        if is_same_line then
-            local pre_selection = string.sub(start_line, 1, pos_start[2])
-            local the_selection = string.sub(start_line, pos_start[2] + 1, pos_end[2] + 1)
-            local post_selection = string.sub(start_line, pos_end[2] + 2)
-
-            local surrounder_len = #surrounder
-
-            local is_removing = vim.startswith(the_selection, surrounder)
-                and vim.endswith(the_selection, surrounder)
-                and #the_selection > (surrounder_len * 2)
-            if is_removing then
-                local sub = string.sub(the_selection, 1 + surrounder_len, -1 - surrounder_len)
-                start_line = pre_selection .. sub .. post_selection
-            else
-                start_line = pre_selection .. surrounder .. the_selection .. surrounder .. post_selection
-            end
-
-            vim.api.nvim_buf_set_lines(0, pos_start[1] - 1, pos_start[1], true, { start_line })
-        else
-            local end_line = vim.api.nvim_buf_get_lines(0, pos_end[1] - 1, pos_end[1], true)[1]
-            local pre_end_line = string.sub(end_line, 1, pos_end[2] + 1)
-            local post_end_line = string.sub(end_line, pos_end[2] + 2)
-
-            local is_post_bold = vim.endswith(pre_end_line, surrounder)
-
-            local pre_start_line = string.sub(end_line, 1, pos_start[2] + 1)
-            local post_start_line = string.sub(end_line, pos_start[2] + 2)
-
-            local is_pre_bold = vim.endswith(pre_start_line, surrounder)
-
-            if is_post_bold and is_pre_bold then
-                -- remove **
-                start_line = pre_start_line .. string.sub(post_start_line, 3)
-                end_line = string.sub(pre_end_line, 1, -2) .. post_end_line
-            else
-                -- add **
-                start_line = pre_start_line .. surrounder .. post_start_line
-                end_line = pre_end_line .. surrounder .. post_end_line
-            end
-
-            vim.api.nvim_buf_set_lines(0, pos_start[1] - 1, pos_start[1], true, { start_line })
-            vim.api.nvim_buf_set_lines(0, pos_end[1] - 1, pos_end[1], true, { end_line })
-        end
+        surrender(pos_start, pos_end, before, after)
     end
 end
 
-M.visual_bold = make_surrounder_function('**')
-M.visual_italic = make_surrounder_function('_')
+M.visual_bold = make_surrounder_function('**', '**')
+M.visual_italic = make_surrounder_function('_', '_')
 
 function M.visual_link()
-    -- {line, col}
     local pos_start = vim.api.nvim_buf_get_mark(0, '<')
-    -- {line, col}
     local pos_end = vim.api.nvim_buf_get_mark(0, '>')
 
-    local start_line = vim.api.nvim_buf_get_lines(0, pos_start[1] - 1, pos_start[1], true)[1]
-
-    local is_same_line = pos_start[1] == pos_end[1]
-
-    if is_same_line then
-        vim.ui.input({ prompt = 'Href:' }, function(href)
-            if href == '' then
-                return
-            end
-
-            local pre_selection = string.sub(start_line, 1, pos_start[2])
-            local the_selection = string.sub(start_line, pos_start[2] + 1, pos_end[2] + 1)
-            local post_selection = string.sub(start_line, pos_end[2] + 2)
-
-            start_line = pre_selection .. '[' .. the_selection .. '](' .. href .. ')' .. post_selection
-
-            vim.api.nvim_buf_set_lines(0, pos_start[1] - 1, pos_start[1], true, { start_line })
-        end)
-    else
-        -- TODO: select multiline links
-        vim.notify('multiline link is not supported', vim.log.levels.ERROR)
-    end
+    vim.ui.input({ prompt = 'Href:' }, function(href)
+        surrender(pos_start, pos_end, '[', ']('..href..')')
+    end)
 end
 
 function M.setup()
