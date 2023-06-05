8b79e4b6d6aba6cac81ef28bc99e5ff7a846270b
Anton Kastritskiy <antonk52@meta.com>
2023-06-05 13:23:59 +0100

    [vim] add nvim spectre for global search and replace similar to coc.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -57,6 +57,28 @@ local plugins = {
         end,
     },
     {
+        'nvim-pack/nvim-spectre', -- global search and replace
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
+        config = function()
+            require('spectre').setup()
+
+            vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").open()<CR>', {
+                desc = "Open Spectre"
+            })
+            vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+                desc = "Search current word"
+            })
+            vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+                desc = "Search current word"
+            })
+            vim.keymap.set('n', '<leader>sf', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+                desc = "Search on current file"
+            })
+        end,
+    },
+    {
         'natecraddock/workspaces.nvim',
         config = function()
             require('antonk52.workspaces').setup()
