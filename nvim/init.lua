f85adb2d6060c0f1c01f9ec80674342c829720d9
Anton Kastritskiy <antonk52@meta.com>
2023-06-06 14:48:21 +0100

    [vim] trouble.nvim to see all diagnostics in a quickfix like split

--- nvim/init.lua
+++ nvim/init.lua
@@ -93,6 +93,23 @@ local plugins = {
             end, { desc = 'Run npm script' })
         end,
     },
+    {
+        'folke/trouble.nvim',
+        opts = {
+            icons = false,
+            fold_open = "v", -- icon used for open folds
+            fold_closed = ">", -- icon used for closed folds
+            indent_lines = false, -- add an indent guide below the fold icons
+            signs = {
+                -- icons / text used for a diagnostic
+                error = "error",
+                warning = "warn",
+                hint = "hint",
+                information = "info"
+            },
+            use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+        },
+    },
     'antonk52/gitignore-grabber.nvim',
     {
         'nvim-treesitter/nvim-treesitter',
