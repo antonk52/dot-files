ed669f034a4cafd3be6bc4ecb312e5adc12f6ce1
Anton Kastritskiy <antonk52@meta.com>
2023-11-06 15:41:40 +0000

    [vim] remove unused

--- nvim/lua/antonk52/notes.lua
+++ nvim/lua/antonk52/notes.lua
@@ -48,10 +48,7 @@ function M.setup()
 
     vim.api.nvim_create_user_command('NoteNext', M.note_next, {})
     vim.api.nvim_create_user_command('NotePrev', M.note_prev, {})
-    vim.api.nvim_create_user_command('NoteNew', M.note_new, {})
     vim.api.nvim_create_user_command('NoteWeek', M.note_week_now, {})
-    vim.api.nvim_create_user_command('NoteWeekNext', M.note_week_next, {})
-    vim.api.nvim_create_user_command('NoteWeekPrev', M.note_week_prev, {})
 
     local function set_goto_mapping()
         vim.keymap.set('n', '<leader>t', M.goto_today, { buffer = 0 })
@@ -82,11 +79,15 @@ function M.setup()
 end
 
 function M.list_notes()
-    local cmd = 'silent ! fd -t f'
-    local lines = vim.split(vim.trim(vim.fn.split(vim.fn.execute(cmd), '\n\n')[2]), '\n', {})
-    table.sort(lines)
+    local files = {}
+    for name, type in vim.fs.dir(vim.fn.expand(vim.env.NOTES_PATH), { depth = 100 }) do
+        if type == 'file' then
+            table.insert(files, name)
+        end
+    end
+    table.sort(files)
 
-    return lines
+    return files
 end
 
 function M.indexOf(value, tab)
@@ -99,28 +100,14 @@ function M.indexOf(value, tab)
     return -1
 end
 
-function M.prev_index(val, tab)
-    local prev_i = nil
-
-    for i, v in ipairs(tab) do
-        if v ~= val then
-            prev_i = i
-        else
-            return prev_i
-        end
-    end
-
-    return -1
-end
-
 function M.note_prev()
     local lines = M.list_notes()
     local path = vim.fn.expand('%')
-    local index = M.indexOf(vim.startswith(path, './') and path or './' .. path, lines)
+    local index = M.indexOf(path, lines)
 
     local prev_note = lines[index - 1]
     if prev_note == nil then
-        print('prev note does not exist')
+        vim.notify('prev note does not exist', vim.log.levels.ERROR)
     else
         vim.cmd('edit ' .. prev_note)
     end
@@ -129,65 +116,19 @@ end
 function M.note_next()
     local lines = M.list_notes()
     local path = vim.fn.expand('%')
-    local index = M.indexOf(vim.startswith(path, './') and path or './' .. path, lines)
+    local index = M.indexOf(path, lines)
 
     local next_note = lines[index + 1]
     if next_note == nil then
-        print('next note does not exist')
+        vim.notify('next note does not exist', vim.log.levels.ERROR)
     else
         vim.cmd('edit ' .. next_note)
     end
-end
-
-function M.note_new()
-    -- 'YYYY/MM/DD'
-    local date = vim.fn.strftime('%Y/%m/%d')
-    vim.cmd('edit ' .. date .. '.md')
 end
 
 function M.note_week_now()
     local week_num = os.date('%Y/%m/week_%V')
     vim.cmd('edit ' .. week_num .. '.md')
-end
-
-function M.note_week_next()
-    local date_str = os.date('%Y-%m-%V')
-    if type(date_str) ~= 'string' then
-        return
-    end
-    local nums = vim.split(date_str, '-', {})
-    local year = nums[1]
-    local month = nums[2]
-    local week = tonumber(nums[3]) + 1
-    vim.cmd('edit ' .. year .. '/' .. month .. '/week_' .. week .. '.md')
-end
-
-function M.note_week_prev()
-    local date_str = os.date('%Y-%m-%V')
-    if type(date_str) ~= 'string' then
-        return
-    end
-    local nums = vim.split(date_str, '-', {})
-    local week = tonumber(nums[3]) - 1
-    local loc = vim.fs.find('week_' .. week .. '.md', {})
-    if #loc > 0 then
-        vim.cmd('edit ' .. loc[1])
-    else
-        vim.notify('Previous week note is not found', vim.log.levels.ERROR)
-    end
-end
-
-function M.week_next()
-    local lines = M.list_notes()
-    local path = vim.fn.expand('%')
-    local index = M.indexOf(vim.startswith(path, './') and path or './' .. path, lines)
-
-    local next_note = lines[index + 1]
-    if next_note == nil then
-        print('next note does not exist')
-    else
-        vim.cmd('edit ' .. next_note)
-    end
 end
 
 return M
