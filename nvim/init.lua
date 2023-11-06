40eb3d10e858eeb4133adbbedb3880fb0c3bc059
Anton Kastritskiy <antonk52@meta.com>
2023-11-06 16:11:01 +0000

    [vim] remove devicons plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -269,7 +269,6 @@ local plugins = {
         version = '*',
         dependencies = {
             'SmiteshP/nvim-navic',
-            'nvim-tree/nvim-web-devicons',
         },
         config = function()
             require('barbecue').setup({
