0d127a4edd4f280231e5860e2125eb0455e0e62b
Anton Kastritskiy <antonk52@meta.com>
2023-11-08 17:43:01 +0000

    [npm] missing language servers

--- scripts/prepare-npm.sh
+++ scripts/prepare-npm.sh
@@ -18,6 +18,7 @@ npm config set --init-author-url "https://github.com/antonk52"
 
 # install global node dependencies
 
+npm i -g cssmodules-language-server
 npm i -g lerna    # monorepo management
 npm i -g n        # switch node versions
 npm i -g neovim   # editor dependency
@@ -25,6 +26,7 @@ npm i -g npmrc    # switch between different npmrc
 npm i -g pkg      # bundle node cli into an executable
 npm i -g serve    # local web server for static files
 npm i -g synd     # rsync wrapper
+npm i -g tailwindcss-language-server
 npm i -g typescript-language-server
 npm i -g veendor  # install deps as an archive
 npm i -g vscode-langservers-extracted # html/json/css/eslint LSPs
