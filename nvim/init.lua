9271842cbf8edf3802d85723cb269f76682d2b1b
Anton Kastritskiy <antonk52@meta.com>
2024-02-07 13:01:37 +0000

    [vim] toggle term colors as a command

--- nvim/init.lua
+++ nvim/init.lua
@@ -697,6 +697,7 @@ vim.keymap.set('n', '<Right>', '<cmd>next<CR>')
 -- Commands {{{1
 local commands = {
     ToggleNumbers = 'set number! relativenumber!',
+    ToggleTermColors = 'set termguicolors!',
     Reroot = function()
         require('antonk52.root').reroot()
     end,
