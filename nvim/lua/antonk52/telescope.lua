1abc2fb81b56964aa2a3addc2b5b25982b9c2eac
antonk52 <halloy52@gmail.com>
2024-02-18 22:21:28 +0000

    [vim] use builtin telescope buffer fuzzy line picker

--- nvim/lua/antonk52/telescope.lua
+++ nvim/lua/antonk52/telescope.lua
@@ -25,7 +25,6 @@ function M.action_meta_telescope()
         end
     end)
 end
-
 function M.action_git_picker()
     vim.ui.select(
         vim.tbl_filter(function(x)
@@ -77,69 +76,11 @@ local function get_nongit_ignore_patterns()
     end
 end
 
--- similar to `telescope.builtin.current_buffer_fuzzy_find`
--- but does not use treesitter for highlighting
--- jumps to the first char from the search (similar to default `/`)
-
--- once this PR gets merged
--- https://github.com/nvim-telescope/telescope.nvim/pull/2909
--- this should be replaced with
--- require('telescope.builtin').current_buffer_fuzzy_find({
---   skip_empty_lines = true,
---   results_ts_highlight = false, -- no highlighting for results
--- })
 function M.action_buffer_lines()
-    local filename = vim.fn.expand(vim.api.nvim_buf_get_name(0))
-    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-    local lines_with_numbers = {}
-    for lnum, line in ipairs(lines) do
-        table.insert(lines_with_numbers, {
-            lnum = lnum,
-            bufnr = 0,
-            filename = filename,
-            text = line,
-        })
-    end
-
-    local column = 0
-    local action_set = require('telescope.actions.set')
-    local action_state = require('telescope.actions.state')
-    require('telescope.pickers')
-        .new({}, {
-            prompt_title = 'Buffer lines:',
-            finder = require('telescope.finders').new_table({
-                results = lines_with_numbers,
-                entry_maker = require('telescope.make_entry').gen_from_buffer_lines({}),
-            }),
-            sorter = require('telescope.config').values.generic_sorter(),
-            attach_mappings = function()
-                action_set.select:enhance({
-                    pre = function(bufnr)
-                        local selection = action_state.get_selected_entry()
-                        if not selection then
-                            return
-                        end
-                        local current_picker = action_state.get_current_picker(bufnr)
-                        local searched_for = action_state.get_current_line()
-                        local highlighted = current_picker.sorter:highlighter(searched_for, selection.ordinal)
-                        column = highlighted[1]
-                    end,
-                    post = function()
-                        local selection = action_state.get_selected_entry()
-                        if not selection then
-                            return
-                        end
-
-                        vim.api.nvim_win_set_cursor(0, { selection.lnum, column or 0 })
-                        -- center line on the screen
-                        vim.api.nvim_feedkeys('zz', 'n', false)
-                    end,
-                })
-
-                return true
-            end,
-        })
-        :find()
+    require('telescope.builtin').current_buffer_fuzzy_find({
+        skip_empty_lines = true,
+        results_ts_highlight = false, -- no highlighting for results
+    })
 end
 
 function M.action_smart_vcs_files()
