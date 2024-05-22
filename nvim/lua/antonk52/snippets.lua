15bf4166767914afc76500db10216fa3c74f7d48
antonk52 <halloy52@gmail.com>
2024-05-22 22:47:39 +0100

    [vim] simplify custom snippets

--- nvim/lua/antonk52/snippets.lua
+++ nvim/lua/antonk52/snippets.lua
@@ -1,4 +1,3 @@
-local cmp = require('cmp')
 local M = {}
 
 ---@param trigger string
@@ -101,62 +100,32 @@ local function get_buf_snips()
     return snips
 end
 
-local function get_snippet()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    local cur_line = vim.api.nvim_buf_get_lines(0, line - 1, line, true)
-    local line_pre_cursor = cur_line[1]:sub(1, col)
-
-    for _, s in ipairs(get_buf_snips()) do
-        if vim.endswith(line_pre_cursor, s.trigger) then
-            return s.trigger, s.body, line, col
-        end
-    end
-
-    return nil
-end
-
-local function expand_under_cursor()
-    local trigger, body, line, col = get_snippet()
-    if not trigger or not line or not col then
-        return false
-    end
-    -- remove trigger
-    vim.api.nvim_buf_set_text(0, line - 1, col - #trigger, line - 1, col, {})
-    vim.api.nvim_win_set_cursor(0, { line, col - #trigger })
-
-    vim.snippet.expand(body)
-    return true
-end
-
 -- cmp source for snippets to show up in completion menu
 function M.register_source()
     local cmp_source = {}
-    cmp_source.new = function()
-        return setmetatable({ cache = {} }, { __index = cmp_source })
-    end
-    cmp_source.complete = function(self, _, callback)
-        local bufnr = vim.api.nvim_get_current_buf()
-        if not self.cache[bufnr] then
+    local cache = {}
+    function cmp_source.complete(_, _, callback)
+        local filetype = vim.bo.filetype
+        if not cache[filetype] then
             local completion_items = vim.tbl_map(function(s)
-                return {
+                ---@type lsp.CompletionItem
+                local item = {
                     word = s.trigger,
                     label = s.trigger,
-                    kind = cmp.lsp.CompletionItemKind.Snippet,
+                    kind = vim.lsp.protocol.CompletionItemKind.Snippet,
+                    insertText = s.body,
+                    insertTextFormat = vim.lsp.protocol.InsertTextFormat.Snippet,
                 }
+                return item
             end, get_buf_snips())
 
-            self.cache[bufnr] = completion_items
-            callback(completion_items)
+            cache[filetype] = completion_items
         end
 
-        callback(self.cache[bufnr])
+        callback(cache[filetype])
     end
 
-    function cmp_source:execute(completion_item, callback)
-        expand_under_cursor()
-        callback(completion_item)
-    end
-    require('cmp').register_source('snp', cmp_source.new())
+    require('cmp').register_source('snp', cmp_source)
 end
 
 return M
