3535ec0485cacab6120a7f72040a20822b91f0b8
Anton Kastritskiy <antonk52@meta.com>
2024-05-28 10:52:54 +0100

    [vim] experimental interactive git status buffer

--- nvim/lua/antonk52/git_utils.lua
+++ nvim/lua/antonk52/git_utils.lua
@@ -63,12 +63,76 @@ local function download_gitignore_file()
     end)
 end
 
+local function git_status()
+    local function update_status(buf)
+        local out = vim.system({ 'git', 'status', '--porcelain' }, nil):wait()
+        if out.code ~= 0 then
+            return vim.notify('Failed to run git status\n' .. out.stderr, vim.log.levels.ERROR)
+        end
+        local lines = vim.split(out.stdout, '\n')
+        vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+        for i, line in ipairs(lines) do
+            local mode = vim.trim(line:sub(1, 2))
+            local mode_to_hl = {
+                ['??'] = 'DiffNewFile',
+                ['M'] = 'DiffChange',
+                ['D'] = 'DiffDelete',
+                ['A'] = 'DiffAdd',
+                ['R'] = 'DiffRemoved',
+            }
+            if mode_to_hl[mode] then
+                vim.api.nvim_buf_add_highlight(buf, -1, mode_to_hl[mode], i - 1, 0, 2)
+            end
+        end
+    end
+
+    local buf = vim.api.nvim_create_buf(false, true)
+    update_status(buf)
+
+    -- initial buffer setup
+    vim.api.nvim_set_option_value('buftype', 'nofile', { buf = buf })
+    vim.api.nvim_set_option_value('bufhidden', 'wipe', { buf = buf })
+    vim.api.nvim_set_option_value('swapfile', false, { buf = buf })
+    vim.api.nvim_buf_set_name(buf, 'git status')
+
+    -- open a new window and set it to the buffer
+    vim.cmd.split()
+    vim.api.nvim_win_set_buf(0, buf)
+    vim.api.nvim_win_set_height(0, 10)
+
+    -- add keymaps
+    do
+        local char_to_command = {
+            a = 'add',
+            r = 'reset',
+            d = 'rm',
+        }
+        for char, command in pairs(char_to_command) do
+            vim.keymap.set('n', char, function()
+                local line = vim.api.nvim_get_current_line()
+                local path = line:match('..%s*(.*)')
+
+                local out = vim.system({ 'git', command, path }, nil):wait()
+                if out.code ~= 0 then
+                    return vim.notify(
+                        'Failed to run "git ' .. command .. ' ' .. path .. '"\n' .. out.stderr,
+                        vim.log.levels.ERROR
+                    )
+                end
+
+                update_status(buf)
+            end, { desc = 'git ' .. command .. ' file', buf = buf })
+        end
+    end
+end
+
 function M.setup()
     vim.api.nvim_create_user_command(
         'GitAddPatch',
         run_cmd_and_exit('git add --patch'),
         { nargs = 0, desc = 'git add --patch' }
     )
+    vim.api.nvim_create_user_command('GitStatus', git_status, { nargs = 0, desc = 'git status with smarts' })
     vim.api.nvim_create_user_command(
         'GitAddPatchFile',
         run_cmd_and_exit('git add --patch %'),
