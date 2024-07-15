722f6caa5bff8006e589c3660d9a413c7ed5f940
Anton Kastritskiy <antonk52@meta.com>
2024-07-15 22:19:55 +0100

    [scripts] remove unused

--- scripts/prepare-xdg.sh
+++ scripts/prepare-xdg.sh
@@ -2,7 +2,7 @@
 
 mkdir -p "${XDG_CONFIG_HOME:=$HOME/.config}"
 
-mkdir -p "$XDG_CONFIG_HOME"/{coc,docker,gh,htop,less,zsh}
+mkdir -p "$XDG_CONFIG_HOME"/{docker,gh,less,zsh}
 
 DOTS="$HOME"/dot-files
 
@@ -10,9 +10,7 @@ DOTS="$HOME"/dot-files
 [ ! -L "$XDG_CONFIG_HOME"/nvim ] && ln -s "$DOTS"/nvim "$XDG_CONFIG_HOME"/nvim
 [ ! -L "$XDG_CONFIG_HOME"/ghostty ] && ln -s "$DOTS"/ghostty "$XDG_CONFIG_HOME"/ghostty
 [ ! -L "$XDG_CONFIG_HOME"/tmux ] && ln -s "$DOTS"/tmux "$XDG_CONFIG_HOME"/tmux
-[ ! -L "$XDG_CONFIG_HOME"/wezterm ] && ln -s "$DOTS"/wezterm "$XDG_CONFIG_HOME"/wezterm
 [ ! -L "$XDG_CONFIG_HOME"/karabiner ] && ln -s "$DOTS"/karabiner "$XDG_CONFIG_HOME"/karabiner
-[ ! -L "$XDG_CONFIG_HOME"/kitty ] && ln -s "$DOTS"/kitty "$XDG_CONFIG_HOME"/kitty
 [ ! -L "$XDG_CONFIG_HOME"/alacritty.toml ] && ln -s "$DOTS"/alacritty.toml "${XDG_CONFIG_HOME}"/alacritty.toml
 
 [ ! -L "$HOME"/.zshrc ] && ln -s "$DOTS"/.zshrc "$HOME"/.zshrc
