e81c0252af89a553fce7d0fba8bf0978ae35dbd6
Anton Kastritskiy <antonk52@meta.com>
2024-07-30 17:48:01 +0100

    [vim] markdown gO support for buffer headings

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -18,3 +18,20 @@ vim.keymap.set('n', 'k', 'gk', { buffer = 0 })
 vim.opt_local.spell = true
 vim.opt_local.spellsuggest = 'best'
 vim.bo.spelllang = 'ru_ru,en_us'
+vim.keymap.set('n', 'gO', function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    local items = {}
+    for lnum, text in ipairs(lines) do
+        if vim.startswith(text, '#') then
+            table.insert(items, { lnum = lnum, text = text, bufnr = bufnr })
+        end
+    end
+
+    if #items == 0 then
+        return vim.notify('No items found', vim.log.levels.WARN)
+    end
+
+    vim.fn.setqflist({}, ' ', { title = 'Buffer headings', items = items })
+    vim.cmd.copen()
+end, { buffer = 0 })
