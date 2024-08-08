8e55ebd43c6dc9098d54c2ac48b20932913bf357
antonk52 <halloy52@gmail.com>
2024-08-08 23:04:21 +0100

    [vim] ts textobjects handle comments

--- nvim/lua/antonk52/treesitter_textobjects.lua
+++ nvim/lua/antonk52/treesitter_textobjects.lua
@@ -86,11 +86,11 @@ local function _get_fn_bounds(outer)
 
     return {
         start = {
-            line = start_line,
+            line = start_line + 1,
             col = start_col,
         },
         endd = {
-            line = end_line,
+            line = end_line + 1,
             col = end_col,
         },
         node = outer and fn_node or prev_node,
@@ -109,7 +109,78 @@ function M.select_fn(outer)
         -- to make <C-o> work
         vim.cmd("normal! m'")
 
-        require('nvim-treesitter.ts_utils').update_selection(0, range.node, 'v')
+        -- Set the cursor to the start position
+        vim.api.nvim_buf_set_mark(0, '<', range.start.line, range.start.col, {})
+        -- Set the cursor to the end position
+        -- have to not include last character since it is \n
+        vim.api.nvim_buf_set_mark(0, '>', range.endd.line, range.endd.col, {})
+
+        -- Enter visual mode and select the range
+        vim.cmd('normal! gv')
+    end
+end
+
+---@param outer boolean
+function M.select_comment(outer)
+    return function()
+        local has_treesitter_parser = pcall(vim.treesitter.get_parser, 0, vim.bo.filetype)
+        if not has_treesitter_parser then
+            return
+        end
+
+        local node = vim.treesitter.get_node()
+        if not node then
+            return
+        end
+
+        while node do
+            if node:type() == 'comment' then
+                break
+            end
+            node = node:parent()
+        end
+
+        if not node then
+            return
+        end
+
+        local start_node, end_node = node, node
+
+        while start_node do
+            local prev = start_node:prev_sibling()
+            if not prev or prev:type() ~= 'comment' then
+                break
+            end
+            start_node = prev
+        end
+
+        while end_node do
+            local next = end_node:next_sibling()
+            if not next or next:type() ~= 'comment' then
+                break
+            end
+            end_node = next
+        end
+
+        -- select comment body for start and end nodes
+        if not outer then
+            -- 0 comment marker
+            -- 1 comment body
+            start_node = start_node:child_count() > 0 and start_node:child(1) or start_node
+            end_node = end_node:child_count() > 0 and end_node:child(1) or end_node
+        end
+
+        local start_line, start_col, end_line, end_col = start_node:range()
+        local _, _, end_line, end_col = end_node:range()
+
+        -- Set the cursor to the start position
+        vim.api.nvim_buf_set_mark(0, '<', start_line, start_col, {})
+        -- Set the cursor to the end position
+        -- have to not include last character since it is \n
+        vim.api.nvim_buf_set_mark(0, '>', end_line, end_col - 1, {})
+
+        -- Enter visual mode and select the range
+        vim.cmd('normal! gv')
     end
 end
 
@@ -117,9 +188,8 @@ function M.setup()
     vim.keymap.set({ 'o', 'x' }, 'if', M.select_fn(false), { desc = 'c/d/v function body' })
     vim.keymap.set({ 'o', 'x' }, 'af', M.select_fn(true), { desc = 'c/d/v function' })
 
-    -- TODO vic/vaf
-    -- TODO dic/dac
-    -- TODO cic/caf
+    vim.keymap.set({ 'o', 'x' }, 'ic', M.select_comment(false), { desc = 'c/d/v comment content' })
+    vim.keymap.set({ 'o', 'x' }, 'ac', M.select_comment(true), { desc = 'c/d/v comment' })
 end
 
 return M
