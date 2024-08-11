261105949fe46cb391d1bec8fd33b9cb39c283aa
antonk52 <halloy52@gmail.com>
2024-08-11 11:07:17 +0100

    [vim] fix custom textobjects

--- nvim/lua/antonk52/treesitter_textobjects.lua
+++ nvim/lua/antonk52/treesitter_textobjects.lua
@@ -1,34 +1,44 @@
 local M = {}
 
 local FUNCTION_NODES = {
+    'arrow_function',
+    'constructor',
+    'function',
     'function_declaration',
     'function_definition',
     'function_expression',
-    'function',
-    'arrow_function',
+    'method',
     'method_declaration',
     'method_definition',
-    'method',
-    'constructor',
 }
+
+local function select_range(start_line, start_col, end_line, end_col)
+    -- to make <C-o> work
+    vim.cmd("normal! m'")
+    -- Set the cursor to the start position
+    vim.api.nvim_buf_set_mark(0, '<', start_line, start_col, {})
+    -- Set the cursor to the end position
+    -- have to not include last character since it is \n
+    vim.api.nvim_buf_set_mark(0, '>', end_line, end_col, {})
+    -- Enter visual mode and select the range
+    vim.cmd('normal! gv')
+end
 
 ---@alias ak_position {line: number, col: number}
 
----@alias ak_range {start: ak_position, endd: ak_position, node: TSNode}
+---@alias ak_range {start: ak_position, endd: ak_position}
 
 ---@param outer boolean
 ---@return ak_range?
 local function _get_fn_bounds(outer)
-    local has_treesitter_parser = pcall(vim.treesitter.get_parser, 0, vim.bo.filetype)
-    if not has_treesitter_parser then
-        return
-    end
-
     ---@type TSNode|nil
     local fn_node = nil
     ---@type TSNode
     local prev_node = nil
-    local node = vim.treesitter.get_node()
+    local ok, node = pcall(vim.treesitter.get_node)
+    if not ok or not node then
+        return
+    end
 
     while node do
         if vim.tbl_contains(FUNCTION_NODES, node:type()) then
@@ -43,14 +53,7 @@ local function _get_fn_bounds(outer)
         or (
             node
             and (function()
-                local children = node:named_children()
-                vim.print({
-                    node = node:type(),
-                    children = vim.tbl_map(function(n)
-                        return n:type()
-                    end, children),
-                })
-                for i, inode in ipairs(children) do
+                for _, inode in ipairs(node:named_children()) do
                     if inode:type() == 'body' then
                         return inode
                     end
@@ -67,11 +70,9 @@ local function _get_fn_bounds(outer)
     if outer then
         start_line, start_col, end_line, end_col = fn_node:range()
     elseif prev_node then
-        -- we don't want to select function body, but all children
+        -- we don't want to select function body, but all its children
         local total_children = prev_node:named_child_count()
-        if total_children == 0 then
-            start_line, start_col, end_line, end_col = prev_node:named_child(0):range()
-        elseif total_children > 0 then
+        if total_children > 0 then
             start_line, start_col = prev_node:named_child(0):range()
             _, _, end_line, end_col = prev_node:named_child(total_children - 1):range()
         else
@@ -93,35 +94,24 @@ local function _get_fn_bounds(outer)
             line = end_line + 1,
             col = end_col,
         },
-        node = outer and fn_node or prev_node,
     }
 end
 
----@param outer boolean
-function M.select_fn(outer)
+---@param select_around boolean
+function M.select_fn(select_around)
     return function()
-        local range = _get_fn_bounds(outer)
+        local range = _get_fn_bounds(select_around)
 
         if not range then
             return
         end
 
-        -- to make <C-o> work
-        vim.cmd("normal! m'")
-
-        -- Set the cursor to the start position
-        vim.api.nvim_buf_set_mark(0, '<', range.start.line, range.start.col, {})
-        -- Set the cursor to the end position
-        -- have to not include last character since it is \n
-        vim.api.nvim_buf_set_mark(0, '>', range.endd.line, range.endd.col, {})
-
-        -- Enter visual mode and select the range
-        vim.cmd('normal! gv')
+        select_range(range.start.line, range.start.col, range.endd.line, range.endd.col)
     end
 end
 
----@param outer boolean
-function M.select_comment(outer)
+---@param select_around boolean
+function M.select_comment(select_around)
     return function()
         local has_ts, node = pcall(vim.treesitter.get_node)
         if not has_ts or not node then
@@ -158,7 +148,7 @@ function M.select_comment(outer)
         end
 
         -- select comment body for start and end nodes
-        if not outer then
+        if not select_around then
             -- 0 comment marker
             -- 1 comment body
             start_node = start_node:child_count() > 0 and start_node:child(1) or start_node
@@ -168,14 +158,7 @@ function M.select_comment(outer)
         local start_line, start_col = start_node:range()
         local _, _, end_line, end_col = end_node:range()
 
-        -- Set the cursor to the start position
-        vim.api.nvim_buf_set_mark(0, '<', start_line + 1, start_col, {})
-        -- Set the cursor to the end position
-        -- have to not include last character since it is \n
-        vim.api.nvim_buf_set_mark(0, '>', end_line + 1, end_col - 1, {})
-
-        -- Enter visual mode and select the range
-        vim.cmd('normal! gv')
+        select_range(start_line + 1, start_col, end_line + 1, end_col - 1)
     end
 end
 
