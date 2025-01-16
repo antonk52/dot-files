811e9824abc37f01de75eea192d4dc008a2a021f
antonk52 <halloy52@gmail.com>
2025-01-16 10:03:44 +0000

    [vim] use git_diff snacks picker

--- nvim/init.lua
+++ nvim/init.lua
@@ -567,6 +567,9 @@ usercmd('GitBrowse', function(x)
     })
 end, { nargs = 0, range = true, desc = 'Open in browser' })
 
+usercmd('PickerGitDiff', ':lua Snacks.picker.git_diff()<cr>', { nargs = 0 })
+usercmd('PickerSnacks', ':lua Snacks.picker.pick()<cr>', { nargs = 0 })
+
 -- fat fingers
 usercmd('W', ':w', { nargs = 0 })
 usercmd('Wq', ':wq', { nargs = 0 })
@@ -640,95 +643,5 @@ vim.defer_fn(function()
         layouts.telescope = copy
 
         layouts.select = vim.tbl_deep_extend('force', {}, copy, { layout = { width = 100 } })
-
-        vim.api.nvim_create_user_command('PickerGitDiff', function()
-            require('snacks.picker.core.picker').new({
-                finder = function()
-                    local output = vim.system({ 'git', 'diff' }):wait().stdout
-                    ---@type snacks.picker.finder.Item[]
-                    local results = {}
-                    ---@type string | nil
-                    local filename = nil
-                    ---@type number | nil
-                    local linenumber = nil
-                    ---@type string[]
-                    local hunk = {}
-
-                    for _, line in ipairs(vim.split(output or '', '\n')) do
-                        -- new file
-                        if vim.startswith(line, 'diff') then
-                            -- Start of a new hunk
-                            if hunk[1] ~= nil then
-                                ---@type snacks.picker.finder.Item
-                                local item =
-                                    { file = filename, pos = { linenumber, 0 }, raw_lines = hunk }
-                                table.insert(results, item)
-                            end
-
-                            filename = line:match('^diff .* a/(.*) b/.*$')
-                            linenumber = nil
-
-                            hunk = {}
-                        elseif vim.startswith(line, '@') then
-                            if filename ~= nil and linenumber ~= nil and #hunk > 0 then
-                                table.insert(
-                                    results,
-                                    { file = filename, pos = { linenumber, 0 }, raw_lines = hunk }
-                                )
-                                hunk = {}
-                            end
-                            -- Hunk header
-                            -- @example "@@ -157,20 +157,6 @@ some content"
-                            local linenr_str = string.match(line, '@@ %-.*,.* %+(.*),.* @@')
-                            linenumber = tonumber(linenr_str)
-                            hunk = {}
-                            table.insert(hunk, line)
-                        else
-                            table.insert(hunk, line)
-                        end
-                    end
-                    -- Add the last hunk to the table
-                    if hunk[1] ~= nil and filename and linenumber and hunk then
-                        table.insert(
-                            results,
-                            { file = filename, pos = { linenumber, 0 }, raw_lines = hunk }
-                        )
-                    end
-
-                    local function get_diff_line_idx(lines)
-                        for i, line in ipairs(lines) do
-                            if vim.startswith(line, '-') or vim.startswith(line, '+') then
-                                return i
-                            end
-                        end
-                        return -1
-                    end
-
-                    for _, v in ipairs(results) do
-                        local diff_line_idx = get_diff_line_idx(v.raw_lines)
-                        diff_line_idx = math.max(
-                            -- first line is header, next one is already handled
-                            diff_line_idx - 2,
-                            0
-                        )
-                        v.pos[1] = v.pos[1] + diff_line_idx
-                    end
-
-                    return results
-                end,
-                preview = function(ctx)
-                    local buf = ctx.preview:scratch()
-
-                    vim.bo[buf].modifiable = true
-                    local lines = vim.split(table.concat(ctx.item.raw_lines, '\n'), '\n')
-                    vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
-                    vim.bo[buf].modifiable = false
-
-                    vim.bo[buf].filetype = 'diff'
-
-                    return true
-                end,
-            })
-        end, { nargs = 0, desc = 'git diff hunk picker' })
     end)
 end, 300)
