93bbd7b653307f70b8a0caeb2492e9968ecc0dfb
antonk52 <halloy52@gmail.com>
2025-01-26 19:21:25 +0000

    [hammerspoon] resize window horizontally commands

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,7 +2,8 @@
 
 hs.console.clearConsole()
 hs.alert.show('Hammerspoon Loaded <tab>+R to reload')
-hs.hotkey.bind({ 'ctrl', 'alt', 'shift' }, 'R', hs.reload)
+local HYPER_KEY = { 'ctrl', 'option', 'shift' }
+hs.hotkey.bind(HYPER_KEY, 'R', hs.reload)
 
 -- ===================================================
 --    Focus between windows in the current workspace
@@ -40,9 +41,11 @@ local function update_current_space_windows()
     STATE.currentSpaceWindows = hs.window.filter.new():setCurrentSpace(true):getWindows()
 end
 
-local f = hs.window.filter.new():setCurrentSpace(true):subscribe({
-    hs.window.filter.windowsChanged,
-}, update_current_space_windows)
+hs.timer.doAfter(0.1, function()
+    local f = hs.window.filter.new():setCurrentSpace(true):subscribe({
+        hs.window.filter.windowsChanged,
+    }, update_current_space_windows)
+end)
 
 -- print(hs.inspect(f))
 local function focusNextWindowInScreen()
@@ -86,5 +89,110 @@ local function focusPreviousWindowInScreen()
 end
 
 -- Bind keys to move focus
-hs.hotkey.bind({ 'ctrl', 'option', 'shift' }, 'j', focusNextWindowInScreen)
-hs.hotkey.bind({ 'ctrl', 'option', 'shift' }, 'k', focusPreviousWindowInScreen)
+hs.hotkey.bind(HYPER_KEY, 'j', focusNextWindowInScreen)
+hs.hotkey.bind(HYPER_KEY, 'k', focusPreviousWindowInScreen)
+
+-- ===================================================
+--   resize windows
+-- ===================================================
+
+-- Define the amount to increase the width
+local RESIZE_DELTA = 50
+local RESIZE_MIN_WIDTH = 300
+
+local resize_utils = {}
+
+---@param frame {x: number, y: number, w: number, h: number}
+---@param screenFrame {x: number, y: number, w: number, h: number}
+---@return 'left' | 'right' | 'center' | nil
+function resize_utils.get_align(frame, screenFrame)
+    if frame.x == 0 then
+        return 'left'
+    elseif frame.x + frame.w == screenFrame.w then
+        return 'right'
+    elseif frame.x + frame.w == screenFrame.w / 2 then
+        return 'center'
+    end
+end
+
+-- Function to increase the width of the focused window by 100 pixels
+local function increase_win_width()
+    -- Get the currently focused window
+    local win = hs.window.focusedWindow()
+    if not win then
+        hs.alert.show('No focused window')
+        return
+    end
+
+    -- Get the current frame of the window
+    local frame = win:frame()
+
+    -- Get the screen's frame to ensure the window doesn't go off-screen
+    local screenFrame = win:screen():frame()
+
+    -- Calculate the new width
+    local new_width = frame.w + RESIZE_DELTA
+
+    if new_width > screenFrame.w then
+        -- hs.alert.show('Max width reached', nil, nil, 0.1)
+        new_width = screenFrame.w
+        frame.x = 0
+    else
+        -- hs.alert.show('Calling align and resize', nil, nil, 0.1)
+        local align = resize_utils.get_align(frame, screenFrame)
+
+        if align == 'center' and screenFrame.w - new_width > RESIZE_DELTA then
+            frame.x = frame.x - RESIZE_DELTA / 2
+        elseif align == 'right' and screenFrame.w - new_width > RESIZE_DELTA then
+            frame.x = frame.x - RESIZE_DELTA
+        else
+            frame.x = 0
+        end
+    end
+
+    -- Set the new frame with the increased width
+    frame.w = new_width
+    win:setFrame(frame, 0)
+end
+
+local function decrease_win_width()
+    -- Get the currently focused window
+    local win = hs.window.focusedWindow()
+    if not win then
+        hs.alert.show('No focused window')
+        return
+    end
+
+    -- Get the current frame of the window
+    local frame = win:frame()
+    -- Get the screen's frame to ensure the window doesn't go off-screen
+    local screenFrame = win:screen():frame()
+
+    -- Calculate the new width
+    local new_width = frame.w - RESIZE_DELTA
+
+    local align = resize_utils.get_align(frame, screenFrame)
+
+    if new_width < RESIZE_MIN_WIDTH then
+        hs.alert.show('Min width reached', nil, nil, 0.1)
+        new_width = RESIZE_MIN_WIDTH
+    else
+        if align == 'center' then
+            frame.x = frame.x + RESIZE_DELTA / 2
+        elseif align == 'right' then
+            frame.x = frame.x + RESIZE_DELTA
+        else
+            frame.x = 0
+        end
+    end
+
+    -- Set the new frame with the increased width
+    frame.w = new_width
+    win:setFrame(frame, 0)
+end
+
+hs.hotkey.bind(HYPER_KEY, 'o', increase_win_width)
+hs.hotkey.bind(HYPER_KEY, 'i', decrease_win_width)
+
+-- Optional: Display a message when Hammerspoon config is loaded successfully
+hs.alert.show('Hammerspoon config loaded')
